dd2513a1d8fde908ff9bea380c3a1a1db329ca4a
Natha Barbosa <NathanBDs35@gmail.com>
2024-11-12 16:41:37 -0400

    Update main.c

--- main.c
+++ main.c
@@ -8,41 +8,38 @@ int main(void) {
     int flag = 1;
     int contador = 0;
     int conf = seleciona_dificuldade();
+    if (conf == 0) { //se o usuario digitar qualquer string que nâo seja "facil", "medio" ou "dificil", pede para digitar novamente
+        do {
+            printf("Por favor, digite uma dificuldade valida\n");
+            conf = seleciona_dificuldade();
+            
+        } while (conf == 0); // nâo sai do loop ate que o usuario digite uma entrada valida
+    }
     int bombas = numero_de_Bombas(conf);
     int n = seleciona_Matriz_Back(conf);
     int ganhou = 0;  // Flag para armazenar se o jogador ganhou ou perdeu
     int** mat = constroi_Matriz_Back(n, bombas);
     char** mat_front = matriz_front(n);
     FILE *logfile = abrir_log();  // Abre o arquivo de log para registrar as informações
-
-    // Registra o início do jogo
-    time_t now;
-    struct tm *info;
-    time(&now);
-    info = localtime(&now);
-    fprintf(logfile, "Data e Hora de início: %s", asctime(info));  // Salva a hora de início no log
+    
 
     while (flag) {
-        printf("Agora digite as coordenadas \"x,y\" de 1 a %d para escolher uma posicao: \n", (n - 2));
-        
-        // Lê as coordenadas x e y no formato "x,y"
-        if (scanf("%d,%d", &x, &y) != 2) {
-            printf("Entrada inválida. Tente novamente com o formato \"x,y\".\n");
-            return 1; // Sai do programa em caso de entrada inválida
-        }
+        printf("Agora digite as coordenadas ""x,y"" de 1 a %d para escolher uma posicao: \n", (n - 2));
+        scanf("%d,%d", &x, &y); // lê as coordenadas
 
         // Verifica se as coordenadas estão fora do escopo
-        while (x < 1 || y < 1 || x > (n - 2) || y > (n - 2)) {
+        while (((x < 1) || (y < 1)) || ((x > (n - 2)) || (y > (n - 2)))) {
             printf("A entrada tem que ser um numero dentro do escopo!\n");
             printf("Digite novamente as coordenadas x,y de 1 a %d para escolher uma posicao: \n", (n - 2));
-            if (scanf("%d,%d", &x, &y) != 2) {
-                printf("Entrada inválida. Tente novamente.\n");
-                return 1; // Sai do programa em caso de entrada inválida
-            }
+            scanf("%d,%d", &x, &y);
         }
 
-        // Registra a coordenada no log
-        fprintf(logfile, "Jogador escolheu a coordenada (%d, %d)\n", x, y);
+        // Verifica se a coordenada já foi escolhida antes
+        if (mat_front[x][y] != 'x') {
+            printf("Boa tentativa, informe uma entrada ainda nao usada\n");
+
+        } else{
+        
         
         // Verifica se a coordenada já foi escolhida antes
         if (mat[x][y] == -1) {
@@ -71,6 +68,8 @@ int main(void) {
             }
         }
     }
+        fprintf(logfile, "Jogador escolheu a coordenada (%d, %d)\n", x, y);
+}
 
     // Registra o término do jogo
     registrar_fim_jogo(logfile, ganhou);
