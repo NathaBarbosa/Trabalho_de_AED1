d41136dd56c7ccfd2a7ec0d397dc64c480951281
Natha Barbosa <NathanBDs35@gmail.com>
2024-11-13 11:44:24 -0500

    Update matriz.c

--- matriz.c
+++ matriz.c
@@ -121,10 +121,11 @@ char **matriz_front(int n) {
     return mat_front;
 }
 
+
 void imprime_front(int n, char** mat_front) {
     for (int i = 1; i < n - 1; i++) {
         for (int j = 1; j < n - 1; j++) {
-            printf("%c ", mat_front[i][j]);
+            printf("%c  ", mat_front[i][j]);
         }
         printf("\n");
     }
@@ -183,59 +184,6 @@ void revelarZeros(int x, int y, int n, char** mat_front, int** mat) {
     revelarZeros(x + 1, y + 1, n, mat_front, mat); // Diagonal inferior direita
 }
 
-FILE* abrir_log() {
-    FILE *logfile = fopen("log.txt", "w");
-    if (logfile == NULL) {
-        printf("Erro ao abrir o arquivo de log.\n");
-        exit(1); // Sai do programa se houver erro ao abrir o arquivo
-    }
-
-    // Obter a data e hora atual
-    time_t agora;
-    struct tm *dataHora;
-    time(&agora);
-    dataHora = localtime(&agora);
-
-    // Escrever no arquivo a data e hora de início do jogo
-    fprintf(logfile, "Início do jogo: %02d/%02d/%04d %02d:%02d:%02d\n", 
-            dataHora->tm_mday, dataHora->tm_mon + 1, dataHora->tm_year + 1900,
-            dataHora->tm_hour, dataHora->tm_min, dataHora->tm_sec);
-
-    return logfile;
-}
-
-// Função para escrever no arquivo de log
-void escrever_log(FILE *logfile, char **mat_front, int n, int x, int y) {
-    // Registra a matriz atual
-    fprintf(logfile, "Matriz atual:\n");
-    for (int i = 1; i < n - 1; i++) {
-        for (int j = 1; j < n - 1; j++) {
-            fprintf(logfile, "%c ", mat_front[i][j]);
-        }
-        fprintf(logfile, "\n");
-    }
-
-}
-void registrar_fim_jogo(FILE *logfile, int ganhou) {
-    time_t agora;
-    struct tm *dataHora;
-    time(&agora);
-    dataHora = localtime(&agora);
-
-    // Escrever no arquivo a data e hora de início do jogo
-    fprintf(logfile, "Fim do jogo: %02d/%02d/%04d %02d:%02d:%02d\n", 
-            dataHora->tm_mday, dataHora->tm_mon + 1, dataHora->tm_year + 1900,
-            dataHora->tm_hour, dataHora->tm_min, dataHora->tm_sec);
-
-    // Escreve a mensagem de vitória ou derrota
-    if (ganhou) {
-        fprintf(logfile, "O jogador ganhou o jogo!\n");
-    } else {
-        fprintf(logfile, "O jogador perdeu o jogo, que pena!\n");
-    }
-
-    fprintf(logfile, "\n");  // Linha em branco para separar entradas no log
-}
 int ganhouOuPerdeu(char **mat_front, int bombas, int n){
     int contador = 0;
     for (int i = 1; i < n - 1; i++) {
@@ -255,4 +203,3 @@ int ganhouOuPerdeu(char **mat_front, int bombas, int n){
     }
         
 }
-
