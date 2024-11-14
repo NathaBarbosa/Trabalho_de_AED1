8c98a92231b0e4b76986b7ff5354cd0cb04851bf
Natha Barbosa <NathanBDs35@gmail.com>
2024-11-14 10:23:38 -0500

    Update log.c

--- log.c
+++ log.c
@@ -26,14 +26,14 @@ void escrever_log(FILE *logfile, char **mat_front, int n, int x, int y) {
     fprintf(logfile, "Matriz atual:\n");
     for (int i = 1; i < n - 1; i++) {
         for (int j = 1; j < n - 1; j++) {
-            fprintf(logfile, "%c ", mat_front[i][j]);
+            fprintf(logfile, "%c  ", mat_front[i][j]);
         }
         fprintf(logfile, "\n");
     }
     fprintf(logfile, "Jogador escolheu a coordenada (%d, %d)\n", x, y);
 }
 
-void registrar_fim_jogo(FILE *logfile, int ganhou) {
+void registrar_fim_jogo(FILE *logfile, int ganhou, int **mat_back, int n) {
     time_t agora;
     struct tm *dataHora;
     time(&agora);
@@ -42,6 +42,18 @@ void registrar_fim_jogo(FILE *logfile, int ganhou) {
     fprintf(logfile, "Fim do jogo: %02d/%02d/%04d %02d:%02d:%02d\n", 
             dataHora->tm_mday, dataHora->tm_mon + 1, dataHora->tm_year + 1900,
             dataHora->tm_hour, dataHora->tm_min, dataHora->tm_sec);
+
+    fprintf(logfile, "Matriz Final:\n");
+    for (int i = 1; i < n - 1; i++) {
+        for (int j = 1; j < n - 1; j++) {
+            if(mat_back[i][j] == -1){
+              fprintf(logfile,"%d  ", mat_back[i][j]);  
+            }else{
+              fprintf(logfile," %d  ", mat_back[i][j]);
+            }
+        }
+        fprintf(logfile, "\n");
+    }
 
     fprintf(logfile, "%s\n", ganhou ? "O jogador ganhou o jogo!" : "O jogador perdeu o jogo!");
     fprintf(logfile, "\n");
